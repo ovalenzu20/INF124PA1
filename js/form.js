0baf5ce98fc4a79d4fa37d5174aa22302ec4223b
Gevorg <gevorgg@uci.edu>
2019-04-22 23:32:36 -0700

    form validation

--- js/form.js
+++ js/form.js
@@ -13,7 +13,7 @@ var inputState = document.querySelector("#inputState");
 
 var inputZip = document.querySelector("#inputZip");
 var shipping = document.querySelector("#shipping");
-
+var form = document.querySelector("#form");
 
 
 var phoneNumber = document.querySelector("#phoneNumber");
@@ -22,6 +22,11 @@ var phoneNumber = document.querySelector("#phoneNumber");
 console.log(`${email.value},${id.value}`)
 // <a href="mailto:Eryka.Adams@Clickdimensions.com?bcc=julie.patrick@clickdimensions.com&subject=RSVP%20for%20New%20Year's%20Eve%20Party%20&body=Hi%20Bob,%0d%0dI%20would%20like%20to%20RSVP%20to%20your%20party%20invitation.%20Here%20are%20my%20details:%0d%0dName:%20Eryka%20Adams%0dNumber%20of%20Guest:%0d" rel="EMAIL">here</a>
 function buildemail(){
+
+
+    if (!form.checkValidity()){
+       return
+    }
     
     var head = `mailto:${email.value}?Subject=CONFRIM%20PRUCHASE%20for%20MOTHEREARTH&body=`
 
